815c3d58acd07b1265bffcfd413e30526ac76efd
Kizmodevs <hakim.courrier@gmail.com>
2024-11-20 23:32:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"])
 
 class Student (BaseModel):
     id : int
